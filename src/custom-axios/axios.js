891f586f999a06f5c386dd774b9286c6c249629c
Jovan Stojcev <stojcevjovan@hotmail.com>
2023-04-03 01:40:19 +0200

    changed endpoint

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://emt-lab-production.up.railway.app/api',
     headers: {
         'Access-Control-Allow-Origin' : '*'
     }
